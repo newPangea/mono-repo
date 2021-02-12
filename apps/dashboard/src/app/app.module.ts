d1a1f25fae1ea5bbc49820329003754c52c8ea54
Felipe Gaete Gonzalez <gaete.felipe.andres@gmail.com>
2021-02-12 19:32:00 -0300

    users component

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -20,9 +20,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, DashboardComponent],
+  declarations: [AppComponent, LoginComponent, DashboardComponent, UsersComponent],
   imports: [
     SharedModule,
     BrowserModule,
--- apps/dashboard/src/app/dashboard/dashboard.module.ts
+++ apps/dashboard/src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from 'dashboard/app/shared/shared.module';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [UsersComponent],
   imports: [CommonModule, DashboardRoutingModule, SharedModule],
 })
 export class AdminModule {}
