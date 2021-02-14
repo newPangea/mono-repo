b2e45b354709ea12d5841ab0f583cc22f54d56ee
Jose G Vargas <ruedagato@gmail.com>
2021-02-14 16:16:03 -0500

    add icon

--- apps/mobile/src/app/welcome/components/success-sign-up/success-sign-up.component.ts
+++ apps/mobile/src/app/welcome/components/success-sign-up/success-sign-up.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pang-success-sign-up',
   templateUrl: './success-sign-up.component.html',
-  styleUrls: ['./success-sign-up.component.scss']
+  styleUrls: ['./success-sign-up.component.scss'],
 })
 export class SuccessSignUpComponent {
+  constructor(private auth: AngularFireAuth, private router: Router)  {}
 
+  logOut() {
+    this.auth.signOut();
+    this.router.navigate(['/welcome'])
+  }
 }
