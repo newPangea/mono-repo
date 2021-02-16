d2fbbc44b67c248607eb5782a436e1fd521be757
lcaloguerea <hellthrash@gmail.com>
2021-02-16 19:53:25 -0300

    correcting build error

--- libs/core/src/lib/services/school.service.ts
+++ libs/core/src/lib/services/school.service.ts
@@ -13,18 +13,18 @@ export class SchoolService {
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
 
   add(school: School) {
-    return this.db.collection(School.REF).doc(school.key).set(Object.assign({}, school));
+    return this.db.collection(FIRESTORE_COLLECTION.school).doc(school.key).set(Object.assign({}, school));
   }
 
   getAll() {
     return this.db
-      .collection<School>(School.REF, (ref) => ref.orderBy('created_at', 'desc'))
+      .collection<School>(FIRESTORE_COLLECTION.school, (ref) => ref.orderBy('created_at', 'desc'))
       .valueChanges();
   }
 
   findSchoolCode(code: string) {
     return this.db
-      .collection<School>(School.REF, (ref) => ref.where('scode', '==', code))
+      .collection<School>(FIRESTORE_COLLECTION.school, (ref) => ref.where('scode', '==', code))
       .valueChanges();
   }
 
