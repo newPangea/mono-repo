9fe33c3da16c0de7818a119dae03226b25a3c38e
Felipe Gaete Gonzalez <gaete.felipe.andres@gmail.com>
2021-03-16 13:26:30 -0300

    validate enabled user

--- apps/mobile/src/app/home/home.component.ts
+++ apps/mobile/src/app/home/home.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { Router } from '@angular/router';
+import { FUNCTION } from '@pang/const';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'pang-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(
+    private angularFireAuth: AngularFireAuth,
+    private angularFn: AngularFireFunctions,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.validateEnabledUser();
+  }
+
+  private isActivedUser(): Promise<boolean> {
+    return new Promise(async (resolve) => {
+      const currentUserUid = (await this.angularFireAuth.currentUser).uid;
+      if (!currentUserUid) {
+        resolve(true);
+      }
+      this.angularFn
+        .httpsCallable(FUNCTION.isActiveAuthUser)({ uid: currentUserUid })
+        .pipe(take(1))
+        .subscribe(
+          (isActive) => resolve(isActive),
+          () => resolve(false),
+        );
+    });
+  }
+
+  private async kickOutUser(): Promise<void> {
+    await this.angularFireAuth.signOut();
+    this.router.navigate(['/']);
+  }
+
+  private async validateEnabledUser(): Promise<void> {
+    const isActiveUser = await this.isActivedUser();
+    if (!isActiveUser) {
+      this.kickOutUser();
+    }
+  }
+}
