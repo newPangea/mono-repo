d659bd144d37d177e717fe2abf0e5b236259e80c
Leo <l.caloguerea@gmail.com>
2021-03-29 12:56:57 -0300

    saving progress

--- apps/mobile/src/app/map/components/map-view/map-view.component.ts
+++ apps/mobile/src/app/map/components/map-view/map-view.component.ts
@@ -1,9 +1,7 @@
-/* eslint-disable quotes */
-import { OnInit, Component } from '@angular/core';
-import * as d3 from 'd3';
-import * as topojson from 'topojson-client';
-import * as d3tile from 'd3-tile';
-import { cluster } from 'd3';
+/// <reference types="@types/googlemaps" />
+import { MapsAPILoader } from '@agm/core';
+import { OnInit, Component, ElementRef, NgZone, ViewChild } from '@angular/core';
+import { mapStyle } from '../../../../assets/globe-data/styles';
 
 @Component({
   selector: 'pang-map-view',
@@ -11,7 +9,43 @@ import { cluster } from 'd3';
   styleUrls: ['./map-view.component.scss'],
 })
 export class MapViewComponent implements OnInit {
+  latitude = 32.7068176;
+  longitude = -117.190456;
+  zoom = 12;
+  progress = false;
+  addres: string;
+  city: string;
+
+  styles = mapStyle;
+
+  private geoCoder: google.maps.Geocoder;
+
+  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) {}
+
   ngOnInit() {
-    console.log('init');
+    console.log('inits');
+    console.log(this.styles);
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      this.geoCoder = new google.maps.Geocoder();
+    });
+  }
+
+  getAddress(latitude: number, longitude: number) {
+    this.geoCoder.geocode({ location: { lat: latitude, lng: longitude } }, (results, status) => {
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 18;
+          this.addres = results[0].formatted_address;
+          this.city = results[0].address_components[3].long_name;
+          this.progress = false;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        this.progress = false;
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    });
   }
 }
--- apps/mobile/src/app/map/map.module.ts
+++ apps/mobile/src/app/map/map.module.ts
@@ -5,11 +5,20 @@ import { MapComponent } from './map.component';
 import { MapViewComponent } from './components/map-view/map-view.component';
 import { SharedModule } from '../shared/shared.module';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AgmCoreModule } from '@agm/core';
 
 const routes: Routes = [{ path: '', component: MapComponent }];
 
 @NgModule({
   declarations: [MapComponent, MapViewComponent],
-  imports: [CommonModule, SharedModule, MatSnackBarModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MatSnackBarModule,
+    RouterModule.forChild(routes),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC9aRHf8SS9ZQ2kLESnX1RqJmbxFmkYZco&libraries=places',
+    }),
+  ],
 })
 export class MapModule {}
