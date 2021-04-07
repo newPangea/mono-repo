d986e5dd618febb0ab2e254b0441a9b1d3376c0b
Jose G Vargas <ruedagato@gmail.com>
2021-04-07 11:11:01 -0500

    add nrx

--- apps/mobile/src/app/app.module.ts
+++ apps/mobile/src/app/app.module.ts
@@ -1,27 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '@pang/mobile/environments/environment';
-import { IonicModule } from '@ionic/angular';
-import { IsUserCompleteGuard } from '@pang/mobile/app/guards/is-user-complete.guard';
-import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireFunctionsModule } from '@angular/fire/functions';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
 import { AlgoliaModule } from '@pang/algolia';
 import { CoreModule } from '@pang/core';
+import { IsUserCompleteGuard } from '@pang/mobile/app/guards/is-user-complete.guard';
+import { environment } from '@pang/mobile/environments/environment';
+
+import { AppComponent } from './app.component';
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/functions';
 import 'firebase/database';
-import { AngularFireMessagingModule } from '@angular/fire/messaging';
-import { HttpClientModule } from '@angular/common/http';
 
 const app = firebase.initializeApp(environment.fire, 'myApp');
 if (environment.emulate) {
@@ -31,43 +38,52 @@ if (environment.emulate) {
   app.database().useEmulator('localhost', 9000);
 }
 
+const routes: Routes = [
+  {
+    path: 'welcome',
+    loadChildren: () => import('./welcome/welcome.module').then((m) => m.WelcomeModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canActivate: [IsUserCompleteGuard],
+  },
+  {
+    path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full',
+  },
+];
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(
-      [
-        {
-          path: 'welcome',
-          loadChildren: () => import('./welcome/welcome.module').then((m) => m.WelcomeModule),
-        },
-        {
-          path: 'home',
-          loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
-          canActivate: [IsUserCompleteGuard],
-        },
-        {
-          path: '',
-          redirectTo: 'welcome',
-          pathMatch: 'full',
-        },
-      ],
-      { initialNavigation: 'enabled' },
-    ),
-    BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.fire, 'myApp'),
+    AlgoliaModule.forRoot({ apiKey: environment.algolia.apiKey, appId: environment.algolia.appId }),
     AngularFireAuthModule,
-    AngularFirestoreModule,
-    AngularFireStorageModule,
     AngularFireFunctionsModule,
     AngularFireMessagingModule,
-    IonicModule.forRoot(),
-    AlgoliaModule.forRoot({
-      apiKey: environment.algolia.apiKey,
-      appId: environment.algolia.appId,
-    }),
+    AngularFireModule.initializeApp(environment.fire, 'myApp'),
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    BrowserModule,
     CoreModule,
+    EffectsModule.forRoot([]),
     HttpClientModule,
+    IonicModule.forRoot(),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      },
+    ),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
