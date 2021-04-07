dc80d8fb54647d7c0c18663e599393561cccabd0
Jose G Vargas <ruedagato@gmail.com>
2021-04-07 10:46:38 -0500

    fix blink connection

--- apps/mobile/src/app/home/pages/notification/notification.component.ts
+++ apps/mobile/src/app/home/pages/notification/notification.component.ts
@@ -1,30 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, TrackByFunction } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 import { ConnectionService } from '@pang/core';
 import { ConnectionInterface } from '@pang/interface';
 import { ConnectionStatus } from '@pang/const';
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'pang-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss'],
 })
-export class NotificationComponent implements OnInit {
-  $pendingConnection: Observable<ConnectionInterface[]>;
+export class NotificationComponent implements OnInit, OnDestroy {
   load = true;
+  pendingConnection: ConnectionInterface[] = [];
+  private subscribe: Subscription;
+  trackByFn: TrackByFunction<ConnectionInterface>;
 
-  constructor(private connection: ConnectionService) {}
+  constructor(private connection: ConnectionService) {
+    this.trackByFn = (index, item) => {
+      return item.key;
+    };
+  }
 
   ngOnInit(): void {
-    this.$pendingConnection = this.connection
-      .getPendingConnections()
-      .pipe(tap(() => (this.load = false)));
+    this.subscribe = this.connection.getPendingConnections().subscribe((value) => {
+      this.pendingConnection = value;
+      this.load = false;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscribe) {
+      this.subscribe.unsubscribe();
+    }
   }
 
   validateRequest(accept: boolean, request: ConnectionInterface) {
+    this.pendingConnection = this.pendingConnection.filter((req) => req.key !== request.key);
     if (accept) {
       this.connection.connection().doc(request.key).update({ status: ConnectionStatus.ACCEPTED });
     } else {
