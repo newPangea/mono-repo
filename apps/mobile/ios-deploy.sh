964c33d4a1e629b30a4d2b6a19e52e32fd1a062e
Jose G Vargas <ruedagato@gmail.com>
2021-04-14 23:31:02 -0500

    v1.0.18

--- apps/mobile/ios-deploy.sh
+++ apps/mobile/ios-deploy.sh
@@ -3,8 +3,16 @@ xcworkspace=$(PWD)/ios/App/App.xcworkspace
 buildPath=$PWD/ios/build
 
 npx cap sync ios
-.$appPath/update-version.sh
+cd ios/App
+OUTPUT=$(./update-version.sh)
+IFS=$'\n' read -rd '' -a split <<<"$OUTPUT"
+version=${split[4]}
+cd ../..
 
-# xcodebuild -workspace $xcworkspace -scheme App -sdk iphoneos -configuration AppStoreDistribution archive -archivePath $buildPath/App.xcarchive
-# xcodebuild -exportArchive -archivePath $buildPath/App.xcarchive -exportOptionsPlist $appPath/exportOption.plist -exportPath $buildPath
-# xcrun altool --upload-app -f "$buildPath/App.ipa" -u ruedagato@gmail.com -p myvh-zphe-rnhs-bysl
+xcodebuild -workspace $xcworkspace -scheme App -sdk iphoneos -configuration AppStoreDistribution archive -archivePath $buildPath/App.xcarchive
+xcodebuild -exportArchive -archivePath $buildPath/App.xcarchive -exportOptionsPlist $appPath/exportOption.plist -exportPath $buildPath
+xcrun altool --upload-app -f "$buildPath/App.ipa" -u ruedagato@gmail.com -p myvh-zphe-rnhs-bysl
+
+git add .
+git commit -m "v$version"
+git push
